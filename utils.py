acf25853dc9d7c331d4d71fa71b0226af0ee7167
Wayu Ragwongsiri <windywayu2000@gmail.com>
2023-02-20 19:20:23 +0700

    update

--- utils.py
+++ utils.py
@@ -449,7 +449,9 @@ def roi_align(batch_of_featuremaps, proposals, size):
     batchlen=proposals.shape[0]
     proposal_count=proposals.shape[1]
     depth=batch_of_featuremaps.shape[-1]
+
     allrois=np.zeros((batchlen,proposal_count,size[0],size[1],depth))
+    #allrois=np.zeros((batchlen,proposal_count,size,depth))
     for image in range(batchlen):
         featuremap=batch_of_featuremaps[image:image+1]
         proposal=proposals[image]
